a262f3e8db72d5e4af554b1cac3e3324b12de72d
G Codes <graciela.mejia1@yahoo.com>
2022-10-15 22:43:37 -0700

    connect to server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //import express
 const express = require("express");
+//connect to MySQL database
+const mysql = require("mysql2");
 
 //PORT designation and app expression
 const PORT = process.env.PORT || 3001;
@@ -8,6 +10,19 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+  {
+    host: "localHost",
+    //your MySQL username,
+    user: "root",
+    //your MySQL password
+    password: "Help_1234!",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
 //route to handle user requests not supported by app
 //default response for any other request (Not Found)

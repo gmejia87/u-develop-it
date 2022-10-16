d5aa6357ed4a5f0d71509aace7b4047a958682aa
G Codes <graciela.mejia1@yahoo.com>
2022-10-15 23:54:57 -0700

    add query to server.js

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
+//query the database to test connection
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 //start express server on port
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}!`);

2588a6f97259ddc011666a73545fd5874c0b3e76
G Codes <graciela.mejia1@yahoo.com>
2022-10-19 13:32:14 -0700

    remove console.log from connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,17 +2,14 @@
 const mysql = require("mysql2");
 
 //connect to database
-const db = mysql.createConnection(
-  {
-    host: "localHost",
-    //your MySQL username,
-    user: "root",
-    //your MySQL password
-    password: "Help_1234!",
-    database: "election",
-  },
-  console.log("Connected to the election database.")
-);
+const db = mysql.createConnection({
+  host: "localHost",
+  //your MySQL username,
+  user: "root",
+  //your MySQL password
+  password: "Help_1234!",
+  database: "election",
+});
 
 //export db
 module.exports = db;
